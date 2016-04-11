87066c27e76afaab382439a78f00834558200ca3
Roy Chu <roygchu@gmail.com>
2016-04-11 11:21:06 -0700

    Slight change to inputs of AvgMeth.2col.pl

--- AvgMeth.2col.pl
+++ AvgMeth.2col.pl
@@ -37,8 +37,8 @@ die "usage: $0
     3) Input BED or GTF column for name of ROI (ex: 3 for bed files) (NA for no name)
     4) Minimum Read Threshold
     5) Minimum File Threshold (Files without NA data)
-    6,7+) Input Percent Methylation Folder Prefix (exclude \"chr\" from the path)
-    8,9+) Input Sample Name (for header of output file)
+    6,8+) Input Percent Methylation Folder Prefix (exclude \"chr\" from the path)
+    7,9+) Input Sample Name (for header of output file)
 " unless @ARGV > 6;
 
 my $outputname = shift(@ARGV);	# Output with average percentage methylation per PMD
